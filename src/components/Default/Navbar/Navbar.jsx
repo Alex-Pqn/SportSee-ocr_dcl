7b28433fd236e40ebd35885f8036190f90ac3dca
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-10-30 14:33:57 +0100

    DefaultNavbar component init

---
+++ src/components/Default/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import './Navbar.scss';
+
+import { Link } from 'react-router-dom';
+
+const DefaultNavbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
+        <li>
+          <Link to="/">Profil</Link>
+        </li>
+        <li>
+          <Link to="/">Réglage</Link>
+        </li>
+        <li>
+          <Link to="/">Communauté</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default DefaultNavbar;
