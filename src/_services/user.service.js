7d0377c6d857e5ec6c2153cb91e8d4debfc11cb6
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-11-06 15:45:46 +0100

    user service init

---
+++ src/_services/user.service.js
@@ -0,0 +1,88 @@
+import { requestOptions } from '../_helpers/request-options';
+import { userRoutes } from '../config/api/user.config';
+
+import UserModel from '../_models/user/user.model';
+import UserActivityModel from '../_models/user/user.activity';
+import UserAverageSessionsModel from '../_models/user/user.average.sessions';
+import UserPerformanceModel from '../_models/user/user.performance';
+
+export const userService = {
+  findOne,
+  findOneActivity,
+  findOneAverageSessions,
+  findOnePerformance,
+};
+
+/**
+ * GET: Find One
+ * Retrieves user.
+ * @param {Number} userId
+ * @returns
+ */
+async function findOne(userId) {
+  return fetch(userRoutes.getUser(userId), requestOptions.get())
+    .then((res) => {
+      if (!res.ok) return Promise.reject('Unable to find one user.');
+      return res.json();
+    })
+    .then((data) => new UserModel(data.data))
+    .catch((err) => {
+      throw err;
+    });
+}
+
+/**
+ * GET: Find One Activity
+ * Retrieves user activity.
+ * @param {Number} userId
+ * @returns
+ */
+async function findOneActivity(userId) {
+  return fetch(userRoutes.getUserActivity(userId), requestOptions.get())
+    .then((res) => {
+      if (!res.ok) return Promise.reject('Unable to find one user activity.');
+      return res.json();
+    })
+    .then((data) => new UserActivityModel(data.data))
+    .catch((err) => {
+      throw err;
+    });
+}
+
+/**
+ * GET: Find One Average Sessions
+ * Retrieves user average sessions.
+ * @param {Number} userId
+ * @returns
+ */
+async function findOneAverageSessions(userId) {
+  return fetch(userRoutes.getUserAverageSessions(userId), requestOptions.get())
+    .then((res) => {
+      if (!res.ok)
+        return Promise.reject('Unable to find one user average sessions.');
+      return res.json();
+    })
+    .then((data) => new UserAverageSessionsModel(data.data))
+    .catch((err) => {
+      throw err;
+    });
+}
+
+/**
+ * GET: Find One Performance
+ * Retrieves user performance.
+ * @param {Number} userId
+ * @returns
+ */
+async function findOnePerformance(userId) {
+  return fetch(userRoutes.getUserPerformance(userId), requestOptions.get())
+    .then((res) => {
+      if (!res.ok)
+        return Promise.reject('Unable to find one user performance.');
+      return res.json();
+    })
+    .then((data) => new UserPerformanceModel(data.data))
+    .catch((err) => {
+      throw err;
+    });
+}
