2a23d9c6e49d42e93f2ffbbd1623522761a9b3a5
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-11-06 15:49:23 +0100

    import user services

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,46 @@
 import './Dashboard.scss';
 
+import { userService } from '../../_services/user.service';
 import { useEffect, useState } from 'react';
 
 import DashboardAside from '../../components/Dashboard/Aside/Aside';
+function Dashboard() {
+  const userId = 12;
+
+  let [user, updateUser] = useState();
+  let [userActivity, updateUserActivity] = useState({});
+  let [userAverageSessions, updateUserAverageSessions] = useState({});
+  let [userPerformance, updateUserPerformance] = useState({});
+
+  let [isUserLoading, updateUserLoading] = useState(true);
+  let [isUserActivityLoading, updateUserActivityLoading] = useState(true);
+  let [
+    isUserAverageSessionsLoading,
+    updateUserAverageSessionsLoading,
+  ] = useState(true);
+  let [isUserPerformanceLoading, updateUserPerformanceLoading] = useState(true);
+
+  useEffect(getData, [userId]);
+
+  function getData() {
+    userService.findOne(userId).then((user) => {
+      updateUser(user);
+      updateUserLoading(false);
+    });
+    userService.findOneActivity(userId).then((userActivity) => {
+      updateUserActivity(userActivity);
+      updateUserActivityLoading(false);
+    });
+    userService.findOneAverageSessions(userId).then((userAverageSessions) => {
+      updateUserAverageSessions(userAverageSessions);
+      updateUserAverageSessionsLoading(false);
+    });
+    userService.findOnePerformance(userId).then((userPerformance) => {
+      updateUserPerformance(userPerformance);
+      updateUserPerformanceLoading(false);
+    });
+  }
+
   return (
     <div className="dashboard">
       <aside className="dashboard__aside">
