173a5904a89b43c6a64f9ff256336649e1d385dc
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-11-06 15:51:03 +0100

    DashboardWelcome component init

---
+++ src/components/Dashboard/Welcome/Welcome.jsx
@@ -0,0 +1,16 @@
+import './Welcome.scss';
+
+const DashboardWelcome = ({ userName }) => {
+  return (
+    <div className="welcome">
+      <div className="welcome__user">
+        <h1>Bonjour {userName}</h1>
+      </div>
+      <div className="welcome__recap">
+        <span>Félicitation ! Vous avez explosé vos objectifs hier 👏</span>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardWelcome;
