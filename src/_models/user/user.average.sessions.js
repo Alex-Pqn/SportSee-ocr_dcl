a4eb4dd022244df3cbee7ca5abfeb42a27abcc3e
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-11-18 16:25:22 +0100

    User Average Sessions Model: iterate sessions and add a day indicator

--- src/_models/user/user.average.sessions.js
+++ src/_models/user/user.average.sessions.js
@@ -1,7 +1,12 @@
 class UserAverageSessionsModel {
   constructor(data) {
     this.userId = data.userId;
-    this.sessions = data.sessions;
+    this.sessions = data.sessions.map((entry) => ({
+      day: { 1: 'L', 2: 'M', 3: 'M', 4: 'J', 5: 'V', 6: 'S', 7: 'D' }[
+        entry.day
+      ],
+      sessionLength: entry.sessionLength,
+    }));
   }
 }
 
