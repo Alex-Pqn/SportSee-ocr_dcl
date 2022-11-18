41578d30f7ddd831c619735d0142c1b7a875d358
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-11-18 16:23:40 +0100

    setup a NODE_ENV variable

--- src/config/api/api.config.js
+++ src/config/api/api.config.js
@@ -1,3 +1,11 @@
 const apiUrl = 'http://localhost:3000';
 
+export const Environments = {
+  production: 'production',
+  development: 'development',
+  tests: 'tests',
+};
+
+export const NODE_ENV = Environments.development;
+
 export const getApi = () => apiUrl;
