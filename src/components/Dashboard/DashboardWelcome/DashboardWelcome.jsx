aa9311fcc7d178ba1bc1b4ee782754ca93cfc9ac
Sokaaa <Alex-Pqn@users.noreply.github.com>
2022-11-18 16:01:42 +0100

    DashboardWelcome component fixs & style

--- src/components/Dashboard/DashboardWelcome/DashboardWelcome.jsx
+++ src/components/Dashboard/DashboardWelcome/DashboardWelcome.jsx
@@ -4,7 +4,9 @@ const DashboardWelcome = ({ userName }) => {
   return (
     <div className="welcome">
       <div className="welcome__user">
-        <h1>Bonjour {userName}</h1>
+        <h1>
+          Bonjour <span>{userName}</span>
+        </h1>
       </div>
       <div className="welcome__recap">
         <span>Félicitation ! Vous avez explosé vos objectifs hier 👏</span>
